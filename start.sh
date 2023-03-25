39f50fe57f9314828a6a2a4308b6dedbc13211f7
jakharamit44 <125469531+jakharamit44@users.noreply.github.com>
2023-03-25 17:26:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Auto_Filter_Bot_Updatesbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/jakharamit44/shubham /shubham
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /shubham
 fi
-cd /Eva
+cd /shubham
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
